2d99aaaf5365666a73194cb5a5e03cdcf7de70ea
Emily0330 <qomolanma0206@gmail.com>
2023-02-28 00:00:24 +0800

    test-of-connecting-BackAndFrontEnd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,7 +29,8 @@ import { PopupResetpwdComponent } from './popup-resetpwd/popup-resetpwd.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/barcode/barcode.component.ts
+++ src/app/barcode/barcode.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+//import {ApiService} from './api.service';
 
 @Component({
   selector: 'app-barcode',
   templateUrl: './barcode.component.html',
   styleUrls: ['./barcode.component.css']
 })
+@Injectable({
+  providedIn: 'root'
+})
 export class BarcodeComponent {
 
+  constructor(private http: HttpClient) {}
+
+  sendData() {
+
+    const data = new HttpParams() //urlencoded
+    .set('name', 'John')
+    .set('age', 30);
+
+    console.log(data);
+    this.http.post('http://localhost:3000/api', data.toString(), {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    }).subscribe(response => {
+      console.log(response);
+    });
+  }
 }
